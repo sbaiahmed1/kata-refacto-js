0d5719bc875fcc48237ca331492162c0de581308
Ahmed Sbai <ahmedclubust@gmail.com>
2022-05-22 08:23:45 -0400

    refacto: Refactoring done!

---
+++ src/gilded_rose.js
@@ -0,0 +1,54 @@
+class Item {
+  constructor(name, sellIn, quality){
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
+class Shop {
+  constructor(items=[]){
+    this.items = items;
+  }
+  updateQuality() {
+    this.items.forEach(item => {
+      if (item.name !== 'Aged Brie' && item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.quality > 0 && item.name !== 'Sulfuras, Hand of Ragnaros') {
+          item.quality -= 1;
+        }
+      } else if (item.quality < 50) {
+        item.quality += 1;
+        if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality += 1;
+          }
+          if (item.sellIn < 6 && item.quality < 50) {
+            item.quality += 1;
+          }
+        }
+      }
+      if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+        item.sellIn -= 1;
+      }
+      if (item.sellIn < 0) {
+        if (item.name !== 'Aged Brie') {
+          if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+            if (item.quality > 0 && item.name !== 'Sulfuras, Hand of Ragnaros') {
+              item.quality -= 1;
+            }
+          } else {
+            item.quality -= item.quality;
+          }
+        } else if (item.quality < 50) {
+          item.quality += 1;
+        }
+      }
+    });
+
+    return this.items;
+  }
+}
+module.exports = {
+  Item,
+  Shop
+}
